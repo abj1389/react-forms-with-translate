4525643a30cbe7653ac5c6f39564b18a00d75d26
abj1389 <abj_13@hotmail.com>
2023-03-23 14:14:05 +0100

    v.1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import RegisterForm from './components/RegisterForm/RegisterForm';
+import React from 'react';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import Spanish from './lang/es.json';
+import French from './lang/fr.json';
+import English from './lang/en.json';
+
+const locale = navigator.language;
+let defaultMessages;
+switch (locale) {
+  case 'fr-FR':
+    defaultMessages = French;
+    break;
+  case 'en-EN':
+    defaultMessages = English;
+    break;
+  case 'es-ES':
+    defaultMessages = Spanish;
+    break;
+  default:
+    defaultMessages = English;
+}
 
 function App() {
+  const [messages, setMessages] = React.useState(defaultMessages);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale={locale} messages={messages}>
+      <div className='App'>
+        <RegisterForm></RegisterForm>
+
+        <h2>
+          <FormattedMessage id='app:language_selector' />
+        </h2>
+        <button onClick={() => setMessages(Spanish)}><FormattedMessage id='app:spanish' /></button>
+        <button onClick={() => setMessages(English)}><FormattedMessage id='app:english' /></button>
+        <button onClick={() => setMessages(French)}><FormattedMessage id='app:french' /></button>
+
+        <select onChange={(event) => setMessages(event.target.value)}>
+          <option value={Spanish}>
+            <FormattedMessage id='app:spanish' />
+          </option>
+          <option value={English}>
+            <FormattedMessage id='app:english' />
+          </option>
+          <option value={French}>
+            <FormattedMessage id='app:french' />
+          </option>
+        </select>
+      </div>
+    </IntlProvider>
   );
 }
 
